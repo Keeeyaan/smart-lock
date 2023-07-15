064e4c361d2bd95314ee98fb969fc180bd7cf0bb
Keeeyaan <keanjieden@gmail.com>
2023-07-15 22:47:47 +0800

    adjust card UI

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -7,26 +7,29 @@ const Card = ({
   rfid: string;
   name: string;
   image: string;
-  permission: string;
+  permission?: string;
 }) => {
   return (
-    <div className="w-[250px] h-auto shadow flex justify-center items-center flex-col p-4">
-      <div className="h-[150px] w-[150px] border border-slate-400 rounded mb-4">
+    <div className="w-[300px] h-[400px] shadow rounded flex justify-center items-center flex-col pb-4">
+      <div className="h-[250px] w-full mb-4">
         <img
           src={`./image/${image}`}
           alt={image}
-          className="w-full h-full object-cover rounded"
+          className="w-full h-full object-cover rounded-t"
         />
       </div>
-      <div className="flex flex-col gap-2">
-        <h1 className="font-semibold text-slate-600">RFID: {rfid}</h1>
-        <h1 className="font-semibold text-slate-600">Name: {name}</h1>
+      <div className="flex flex-col gap-2 m-2 text-center">
+        <h1 className="font-semibold text-slate-600 text-2xl">{name}</h1>
+        <h1 className="font-semibold text-slate-600 text-lg">{rfid}</h1>
+        <h1 className={`${!permission && 'mb-6'}`}></h1>
         <select
           defaultValue={permission}
-          className="px-4 py-1 font-normal text-gray-700 border border-gray-300 rounded outline-none"
+          className={`${
+            !permission && 'hidden'
+          } px-4 py-1 text-gray-700 border font-medium border-gray-300 rounded outline-none`}
         >
-          <option value="restrict">Restrict</option>
-          <option value="unrestrict">Unrestrict</option>
+          <option value="restrict">Restricted</option>
+          <option value="unrestrict">Unrestricted</option>
         </select>
       </div>
     </div>
