ed652982a0c6374fa849371a8e763f38632e8210
Keeeyaan <keanjieden@gmail.com>
2023-07-18 09:38:52 +0800

    centralized export of pages

---
+++ client/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Control } from './Control';
+export { default as Member } from './Member';
+export { default as Logs } from './Logs';
+export { default as Register } from './Register';
